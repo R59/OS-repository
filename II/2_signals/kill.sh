443e6eea0268f331fc37c57f5aa79836f48f6be4
R59 <qazxswedc@e1.ru>
2011-05-29 00:12:23 +0600

    signals

--- II/2_signals/kill.sh
+++ II/2_signals/kill.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-EXIT_SIGNAL=20	# Ctrl+Z (SIGTSTP)
+EXIT_SIGNAL=20  # Ctrl+Z (SIGTSTP)
 
 SIGKILL=9
 SIGSTOP=19
 
 if (($#==0))
 then
-	echo Usage: kill.sh process_name
-	exit
+        echo Usage: kill.sh process_name
+        exit
 fi;
 
-pid=$(pidof $1)
-[[ -z $pid ]] && exit
+./$1&
+pid=$!
 
-echo start
+#pid=$(pidof $1)
+#[[ -z $pid ]] && exit
+
+echo my pid = $!
+sleep 1
 
 for ((sig=0; sig<32; sig++));
 do
-	if ((sig!=SIGKILL && sig!=SIGSTOP && sig!=EXIT_SIGNAL))
-	then
-		echo send signal $sig
-		kill -n $sig $pid
-	fi;
+        if ((sig!=SIGKILL && sig!=SIGSTOP && sig!=EXIT_SIGNAL))
+        then
+                echo send signal $sig
+                kill -n $sig $pid
+        fi;
 done
-
-echo sleep 1.5 second
-sleep 1.5
 
 echo send EXIT_SIGNAL\($EXIT_SIGNAL\)
 kill -n $EXIT_SIGNAL $pid
-
--- II/2_signals/signals.c
+++ II/2_signals/signals.c
@@ -1,84 +1,77 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <errno.h>
 
-#define MY_SIG_EXIT 20
+#define EXIT_SIG 20
 
-int EXIT = 0;
+int pair[2];
+struct datapacket
+{
+        int num, signo, code, mypid, mygid, sender_pid, sender_gid, sender_uid;
+};
 
-void hello_signal(int number);
-void writeIntDec(int x);
+void hello_signal(int number, siginfo_t *info, void *something);
 
 int main()
 {
-	struct sigaction S;
-	S.sa_handler = hello_signal;
-	S.sa_flags = 0;
-	if(sigfillset(&S.sa_mask)==-1)
-	{
-		puts("Can't set sa_mask");
-		return 1;
-	}
+        if(pipe(pair)==-1)
+        {
+                puts("Can't create pipe");
+                return 1;
+        }
 
-	int i;
-	for(i=1; i<32; ++i)
-		if(i!=SIGKILL && i!=SIGSTOP && sigaction(i, &S, NULL))
-		{
-			printf("Can't set signal %d\n", i);
-			return 2;
-		}
+        struct sigaction S;
+        S.sa_sigaction = hello_signal;
+        S.sa_flags = SA_SIGINFO;
+        if(sigfillset(&S.sa_mask)==-1)
+        {
+                puts("Can't set sa_mask");
+                return 2;
+        }
 
-	sigset_t exitmask;
-	if(sigfillset(&exitmask)==-1 || sigdelset(&exitmask, MY_SIG_EXIT))
-	{
-		printf("Can't set exitmask");
-		return 3;
-	}
+        int i;
+        for(i=1; i<32; ++i)
+                if(i!=SIGKILL && i!=SIGSTOP && sigaction(i, &S, NULL))
+                {
+                        printf("Can't set signal %d\n", i);
+                        return 3;
+                }
 
-	while(1)
-		;
+        struct datapacket d;
+        do
+        {
+                int readed = read(pair[0], &d, sizeof(d));
+                if(readed>0)
+                {
+                        printf("Hello, signal %d!\n\tsigno = %d\n\tcode = %d\n", d.num, d.signo, d.code);
+                        printf("\tmy pid = %d\n\tmy gid = %d\n", d.mypid, d.mygid);
+                        printf("\tsender pid = %d\n\tsender gid = %d\n\tsender uid = %d\n", d.sender_pid, d.sender_gid, d.sender_uid);
+                }
+                else if(readed==-1 && errno!=EINTR)
+                {
+                        puts("read error");
+                        return 4;
+                }
+                else
+                        d.num = -1; // number!=EXIT_SIG
+        }
+        while(d.num != EXIT_SIG);
 
-	//puts("Exit");
-	return 0;
+        puts("Exit");
+        return 0;
 }
 
-void hello_signal(int number)
+void hello_signal(int number, siginfo_t *info, void *something)
 {
-	write(1, "Hello, signal ", sizeof("Hello, signal "));
-	writeIntDec(number);
-	write(1, "\n", 1);
-//	printf("Hello, signal %d!\n", number);
-	if(number==MY_SIG_EXIT)
-		exit(0);
-}
-
-void writeIntDec(int x)
-{
-	#define maxlen 9
-
-	char ch;
-	if(x==0)
-	{
-		ch = '0';
-		write(1,&ch,1);
-		return;
-	}
-
-	if(x<0)
-	{
-		ch = '-';
-		write(1,&ch,1);
-		x = -x;
-	}
-
-	char str[maxlen];
-	int i=maxlen;
-	do
-	{
-		str[--i] = '0' + (x%10);
-	}
-	while(x/=10);
-
-	write(1, str+i, maxlen-i);
+        struct datapacket send_data;
+        send_data.num           = number;
+        send_data.signo         = info->si_signo;
+        send_data.code          = info->si_code;
+        send_data.mypid         = getpid();
+        send_data.mygid         = getpgrp();
+        send_data.sender_pid    = info->si_pid;
+        send_data.sender_gid    = getpgid(info->si_pid);
+        send_data.sender_uid    = info->si_uid;
+        write(pair[1], &send_data, sizeof(send_data));
 }
